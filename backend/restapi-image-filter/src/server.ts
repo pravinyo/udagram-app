cfecac088e037779691264ed27964127040ec1b8
pravin <pravinyo12@gmail.com>
2020-03-24 15:55:26 +0530

    image filter service added

---
+++ backend/restapi-image-filter/src/server.ts
@@ -0,0 +1,116 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  const userFilesTracker= new Map<string,string[]>();
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  app.get( "/filteredimage", async ( req, res ) => {
+    
+    if(!req.query.image_url){
+      res.status(422).send("Image Url required");
+    }
+
+    if(!req.query.user_id){
+      res.status(422).send("UserId required");
+    }
+
+    const imageUrl = req.query.image_url;
+    const userId = req.query.user_id;
+    
+    try{
+      filterImageFromURL(imageUrl).then( value => {
+  
+        res.on("finish",() =>{
+          let files:string[] = userFilesTracker.get(userId);
+          
+          if(files == undefined){
+            files = [value];
+          }else{
+            files.push(value);
+          }
+
+          console.log("values: "+files);
+
+          userFilesTracker.set(userId,files);
+        });
+
+        console.log("value is :"+value);
+        res.status(200).sendFile(value);
+
+      },issue => {
+        res.status(422).send("Issue -> "+issue);
+      });
+
+      
+    }catch(reason){
+      res.status(422).send("There is issue in File:"+imageUrl);
+    }
+  });
+
+  //! END @TODO1
+
+  app.get("/deleteAll", async(req, res)=>{
+
+    if(!req.query.user_id){
+      res.status(422).send("UserId required");
+    }
+
+    const userId = req.query.user_id;
+
+    const files  = userFilesTracker.get(userId);
+
+    if(files!=undefined && files.length>0){
+      deleteLocalFiles(files);
+      userFilesTracker.delete(userId);
+    }
+
+    res.status(200).send("File removed");
+
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send(`<h2> How to use it</h2>
+    <p>To delete the files, this is how our url looks like
+    <br><i>http://{{HOST_NAME}}/deleteAll?user_id={{ID}}</i>
+    </p><p>
+    To make request for image processing:<br><i>
+    http://{{HOST_NAME}}/filteredimage?user_id={{ID}}&image_url={{URL}}</i></p>
+    <br><b>Note: Use any random string for userId</b>`)
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
